9169892704182611e18f93534051ab79194a664a
Rifqifauu <120528673+Rifqifauu@users.noreply.github.com>
2025-10-09 09:00:59 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 $koneksi = new mysqli('127.0.0.1', 'root', 'root', 'gizi', 8889);
-
+// $koneksi = new mysqli('127.0.0.1', 'root', '', 'gizi', 3306); // Untuk XAMPP
 if ($koneksi->connect_error) {
     die("Koneksi gagal: " . $koneksi->connect_error);
 }
